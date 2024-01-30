aa17e904939d5a3d022fae417c12d99b2ca0a850
ahmezing <ahmedyousifoy@gmail.com>
2024-01-31 00:18:01 +0300

    configured aws lambda (serverless) with a fastify server

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
+require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-require('dotenv').config();
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import * as serverlessHttp from 'serverless-http';
+import * as fastify from 'fastify';
 
+let lambdaApp: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  if (!lambdaApp) {
+
+    const app = await NestFactory.create(
+      AppModule,
+      new FastifyAdapter({ logger: true }),
+    );
+
+    await app.init();
+    lambdaApp = serverlessHttp(app.getHttpAdapter().getInstance());
+  }
+  return lambdaApp;
 }
-bootstrap();
+
+export const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const app = await bootstrap();
+  return app(event, context);
+};
+
